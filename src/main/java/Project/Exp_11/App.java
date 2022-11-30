0b158627d09216cee37c9011c5283ae061ac37ca
HI <HI@DESKTOP-TDEOMCR>
2022-11-30 09:01:17 -0800

    this is exp11

---
+++ src/main/java/Project/Exp_11/App.java
@@ -0,0 +1,17 @@
+package Project.Exp_11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to devops lab" );
+    }
+    public int add(int a,int b)
+    {
+         return(a+b);    	
+    }
+}
